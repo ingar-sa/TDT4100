a5c0f401030fb0a33d510e995c5ad156ac7c7c82
Ingar Solveigson Asheim <ingara@stud.ntnu.no>
2022-10-26 11:26:17 +0200

    Implemented Menu and MenuRegistry

--- proggovinger/src/main/java/ovinger/oving10/oppgave2/Menu.java
+++ proggovinger/src/main/java/ovinger/oving10/oppgave2/Menu.java
@@ -13,14 +13,56 @@ public class Menu {
 
     private ArrayList<Dish> dishes;
 
-    public Menu() {
-        dishes = new ArrayList<Dish>();
+    //TODO: Check that the arraylist contains at least two dishes
+    public Menu(ArrayList<Dish> dishes) {
+        if (!hasAtLeastTwoDishes(dishes)) {
+            throw new IllegalArgumentException("A menu must have at least two dishes");
+        }
+
+        this.dishes = new ArrayList<>(dishes);
     }
 
     public void addDish(Dish dish) {
         dishes.add(dish);
     }
 
+
+    public void removeDish(String name) {
+        if (!(dishes.size() > 2)) {
+            //TODO: IllegalStateException?
+            throw new IllegalArgumentException("A menu must have at least two dishes");
+        }
+
+        if (!containsDish(name)) {
+            throw new IllegalArgumentException("The menu does not contain a dish with the name " + name);
+        }
+
+        dishes.remove(getDish(name));
+    }
+
+    public ArrayList<Dish> getDishes() {
+        return new ArrayList<>(dishes);
+    }
+
+    public Dish getDish(String name) {
+        //TODO: Is this the correct way of handling this?
+        // We don't necessarily want to crash the program if the dish is not found
+        if (!containsDish(name)) {
+            throw new IllegalArgumentException("The menu does not contain a dish with the name " + name);
+        }
+
+        return dishes.stream().filter(dish -> dish.getName().equals(name)).findFirst().get();
+    }
     
-    
+    public double getTotalPrice() {
+        return dishes.stream().mapToDouble(dish -> dish.getPrice()).sum();
+    }
+
+    private boolean hasAtLeastTwoDishes(ArrayList<Dish> dishes) {
+        return dishes.size() >= 2;
+    }
+
+    private boolean containsDish(String name) {
+        return dishes.stream().anyMatch(dish -> dish.getName().equals(name));
+    }
 }
--- proggovinger/src/main/java/ovinger/oving10/oppgave2/MenuRegistry.java
+++ proggovinger/src/main/java/ovinger/oving10/oppgave2/MenuRegistry.java
@@ -1,5 +1,56 @@
 package ovinger.oving10.oppgave2;
 
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
 public class MenuRegistry {
     
+    private ArrayList<Menu> menus;
+    
+    private ArrayList<Dish> dishes;
+
+    public MenuRegistry() {
+        menus = new ArrayList<Menu>();
+        dishes = new ArrayList<Dish>();
+    }
+
+    public void addMenu(Menu menu) {
+        menus.add(menu);
+    }
+
+    public void addMenu(ArrayList<String> dishNames) {
+        ArrayList<Dish> menuDishes = dishNames.stream().map(name -> getDishFromName(name)).collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+        menus.add(new Menu(menuDishes));
+    }
+
+    public void addDish(Dish dish) {
+        dishes.add(dish);
+    }
+    
+    public Dish getDishFromName(String name) {
+        try {
+            return dishes.stream().filter(dish -> dish.getName().equals(name)).findFirst().get();
+        } catch (NoSuchElementException  e) {
+            throw new IllegalArgumentException("There is no dish with the name " + name);
+        }
+    }
+
+    public ArrayList<Dish> getDishesFromType(String type) {
+        try {
+            return dishes.stream().filter(dish -> dish.getType().equals(type)).collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+        } catch (NoSuchElementException e) {
+            throw new IllegalArgumentException("There are no dishes of the type " + type);
+        }
+    
+    }
+
+    public ArrayList<Menu> getMenusInPriceRange(double min, double max) {
+        try {
+            return menus.stream().filter(menu -> menu.getTotalPrice() >= min && menu.getTotalPrice() <= max).collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+        } catch (NoSuchElementException e) {
+            throw new IllegalArgumentException("There are no menus within the price range " + min + " - " + max);
+        }
+        
+    }
+
 }
