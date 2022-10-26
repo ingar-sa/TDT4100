1611dcf0275125efdf1fd6fc2c988ade5b2e94b9
Ingar Solveigson Asheim <ingara@stud.ntnu.no>
2022-10-26 11:53:26 +0200

    Added javadoc to all files.

--- proggovinger/src/main/java/ovinger/oving10/oppgave2/Menu.java
+++ proggovinger/src/main/java/ovinger/oving10/oppgave2/Menu.java
@@ -1,19 +1,29 @@
 package ovinger.oving10.oppgave2;
 
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
 /**
  * @author Ingar S. Asheim
  * 
  * This class represents a menu. A menu consists
- * of multiple Dish objects.
+ * of multiple Dish objects. It allows the user
+ * to add and remove dishes, and to get the total 
+ * price of the menu
  */
 
 public class Menu {
 
+    /**
+     * The list of dishes in the menu
+     */
     private ArrayList<Dish> dishes;
 
-    //TODO: Check that the arraylist contains at least two dishes
+    /**
+     * The constructor initializes the field
+     * @param dishes The list of dishes in the menu
+     * @throws IllegalArgumentException if the menu does not contain at least two dishes
+     */
     public Menu(ArrayList<Dish> dishes) {
         if (!hasAtLeastTwoDishes(dishes)) {
             throw new IllegalArgumentException("A menu must have at least two dishes");
@@ -22,11 +32,20 @@ public class Menu {
         this.dishes = new ArrayList<>(dishes);
     }
 
+    /**
+     * Adds a dish to the menu
+     * @param dish The dish to add
+     */
     public void addDish(Dish dish) {
         dishes.add(dish);
     }
 
-
+    /**
+     * Removes a dish from the menu
+     * @param dish The dish to remove
+     * @throws NoSuchElementException if the menu does not contain the dish
+     * @throws NoSuchElementException if the menu contains only two dishes
+     */
     public void removeDish(String name) {
         if (!(dishes.size() > 2)) {
             //TODO: IllegalStateException?
@@ -34,34 +53,58 @@ public class Menu {
         }
 
         if (!containsDish(name)) {
-            throw new IllegalArgumentException("The menu does not contain a dish with the name " + name);
+            throw new NoSuchElementException("The menu does not contain a dish with the name " + name);
         }
 
         dishes.remove(getDish(name));
     }
 
+    /**
+     * Returns a list of all the dishes in the menu
+     * @return A list of all the dishes in the menu
+     */
     public ArrayList<Dish> getDishes() {
         return new ArrayList<>(dishes);
     }
 
+    /**
+     * Gets a dish with the name passed as a parameter
+     * @param name name of the dish to get
+     * @return The dish with the name passed as a parameter
+     * @throws NoSuchElementException if the menu does not contain the dish
+     */
     public Dish getDish(String name) {
         //TODO: Is this the correct way of handling this?
         // We don't necessarily want to crash the program if the dish is not found
-        if (!containsDish(name)) {
-            throw new IllegalArgumentException("The menu does not contain a dish with the name " + name);
+        try {
+            return dishes.stream().filter(dish -> dish.getName().equals(name)).findFirst().get();
+        } catch (NoSuchElementException e) {
+            throw new NoSuchElementException("The menu does not contain a dish with the name " + name);
         }
-
-        return dishes.stream().filter(dish -> dish.getName().equals(name)).findFirst().get();
     }
     
+    /**
+     * Returns the total price of the menu
+     * @return The total price of the menu
+     */
     public double getTotalPrice() {
         return dishes.stream().mapToDouble(dish -> dish.getPrice()).sum();
     }
 
+    /**
+     * Checks if the list has at least two dishes
+     * @param dishes List of dishes to check
+     * @return True if the list has at least two dishes, false otherwise
+     */
     private boolean hasAtLeastTwoDishes(ArrayList<Dish> dishes) {
         return dishes.size() >= 2;
     }
 
+    /**
+     * Checks if the menu contains a dish with the name passed as a parameter
+     * @param name The name of the dish to check
+     * @return True if the menu contains a dish with the name passed as a parameter, false otherwise
+     */
     private boolean containsDish(String name) {
         return dishes.stream().anyMatch(dish -> dish.getName().equals(name));
     }
--- proggovinger/src/main/java/ovinger/oving10/oppgave2/MenuRegistry.java
+++ proggovinger/src/main/java/ovinger/oving10/oppgave2/MenuRegistry.java
@@ -3,52 +3,119 @@ package ovinger.oving10.oppgave2;
 import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
+/**
+ * @author Ingar S. Asheim
+ * 
+ * This class represents a registry of menus and dishes.
+ * It allows the user to add and remove menus and dishes.
+ * It also allows the user to find a dish by name, all dishes
+ * of a certain type, and all dishes of a certain price range.
+ */
 public class MenuRegistry {
     
+    /**
+     * The list of menus.
+     */
     private ArrayList<Menu> menus;
-    
+    /**
+     * The list of dishes.
+     */
     private ArrayList<Dish> dishes;
 
+    /**
+     * The constructor initializes the fields.
+     */
     public MenuRegistry() {
         menus = new ArrayList<Menu>();
         dishes = new ArrayList<Dish>();
     }
 
+    /**
+     * Adds a menu to the registry.
+     * @param menu The menu to add
+     */
     public void addMenu(Menu menu) {
         menus.add(menu);
     }
 
+    /**
+     * Creates a menu from the dishes with the names passed as parameters
+     * and adds it to the registry.
+     * @param menu The list of the names of the dishes to add to the menu
+     * @throws IllegalArgumentException if the menu does not contain at least two dishes
+     * @throws NoSuchElementException if the menu does not contain a dish with the name passed as a parameter
+     */
     public void addMenu(ArrayList<String> dishNames) {
-        ArrayList<Dish> menuDishes = dishNames.stream().map(name -> getDishFromName(name)).collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
-        menus.add(new Menu(menuDishes));
+        ArrayList<Dish> menuDishes = new ArrayList<Dish>();
+
+        try {
+            menuDishes = dishNames.stream().map(name -> getDishFromName(name)).collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+        } catch (NoSuchElementException e) {
+            throw new NoSuchElementException("One or more of the dishes does not exist");
+        }
+
+        try {
+            menus.add(new Menu(menuDishes));
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("A menu must have at least two dishes");
+        }
+        
     }
 
+    /**
+     * Adds a dish to the registry.
+     * @param dish The dish to add
+     */
     public void addDish(Dish dish) {
         dishes.add(dish);
     }
     
+    /**
+     * Gets the dish with the name passed as an argument
+     * @param name Name of the dish to get
+     * @return The dish with the name passed as an argument
+     * @throws NoSuchElementException if no dish with the name passed as an argument exists
+     */
     public Dish getDishFromName(String name) {
         try {
             return dishes.stream().filter(dish -> dish.getName().equals(name)).findFirst().get();
         } catch (NoSuchElementException  e) {
-            throw new IllegalArgumentException("There is no dish with the name " + name);
+            throw new NoSuchElementException("There is no dish with the name " + name);
         }
     }
 
+    /**
+     * Gets all dishes of the type passed as an argument
+     * @param type The type of dishes to get
+     * @return A list of all dishes of the type passed as an argument
+     * @throws NoSuchElementException If there are no dishes of the type passed as an argument
+     */
     public ArrayList<Dish> getDishesFromType(String type) {
         try {
             return dishes.stream().filter(dish -> dish.getType().equals(type)).collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
         } catch (NoSuchElementException e) {
-            throw new IllegalArgumentException("There are no dishes of the type " + type);
+            throw new NoSuchElementException("There are no dishes of the type " + type);
         }
     
     }
 
+    /**
+     * Gets all menus within the price range passed as arguments    
+     * @param minPrice The minimum price of the menus to get
+     * @param maxPrice The maximum price of the menus to get
+     * @return A list of all the menus within the price range
+     * @throws IllegalArgumentException if minPrice is greater than maxPrice or if minPrice is negative
+     * @throws NoSuchElementException if there are no menus within the price range
+     */
     public ArrayList<Menu> getMenusInPriceRange(double min, double max) {
+        if (!(min < max) && !(min >= 0)) {
+            throw new IllegalArgumentException("The minimum price must be less than the maximum price and must be greater than or equal to zero");
+        }
+
         try {
             return menus.stream().filter(menu -> menu.getTotalPrice() >= min && menu.getTotalPrice() <= max).collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
         } catch (NoSuchElementException e) {
-            throw new IllegalArgumentException("There are no menus within the price range " + min + " - " + max);
+            throw new NoSuchElementException("There are no menus within the price range " + min + " - " + max);
         }
         
     }
